ee1e167651993098833cc84f06f72dd800cda459
Mark Tanui <marktanui75@gmail.com>
2021-09-27 18:56:36 +0300

    fixed

--- run.c
+++ run.c
@@ -25,10 +25,9 @@ void run(char (*refer)[80])
 		{"mod", module},
 		{"pchar", pchar},
 		{"pstr", pstr},
-		{"rotl", rotl},
 		{NULL, NULL},
 	};
-	for (j = 0; j < 15; j++)
+	for (j = 0; j < 14; j++)
 	{
 		if (strcmp(funcs[j].opcode, refer[0]) == 0)
 		{
@@ -40,7 +39,7 @@ void run(char (*refer)[80])
 		{
 			nop(&(vari.mystack), vari.line_number);
 		}
-		else if (j == 14)
+		else if (j == 13)
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n",
 				vari.line_number, refer[0]);
